2c0c03e8f2a6992f66e9a96c671925f1485ec3d8
stas <garanzha.s.a@gmail.com>
2021-12-01 22:05:12 +0300

    Add image preview to the admin panel

--- spaceflights/admin.py
+++ spaceflights/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from spaceflights.models import Order, Spaceship
 
 
+
 @admin.register(Spaceship)
 class SpaceShipAdmin(admin.ModelAdmin):
-    list_display = ['owner', 'name', 'flight_price', 'capacity']
+    list_display = [
+        'picture_preview', 'owner', 'name', 'flight_price', 'capacity'
+    ]
+    readonly_fields = ['picture_preview', ]
+
+    @admin.display(description='Превью')
+    def picture_preview(self, obj):
+        return format_html(
+            '<img src="{}"'
+            'style="object-fit: cover; width:200px; height:150px;" />',
+            obj.image.url
+        )
 
 
 @admin.register(Order)
