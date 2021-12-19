7ac61c387614498f622515d15f067d29daae6ce1
stas <garanzha.s.a@gmail.com>
2021-12-19 21:50:11 +0300

    Add currency field to form, update form to model form

--- spaceflights/forms.py
+++ spaceflights/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from django.core.validators import MinValueValidator
 from django.forms.widgets import DateInput, RadioSelect
 
-from .models import Spaceship
+from .models import Order
 
 
 def get_min_date():
@@ -12,26 +12,39 @@ def get_min_date():
     return datetime.date.today() + datetime.timedelta(weeks=4)
 
 
-class OrderForm(forms.Form):
-    ship = forms.ModelChoiceField(
-        queryset=Spaceship.objects,
-        label='На чем летим',
-        widget=RadioSelect
-    )
+class OrderForm(forms.ModelForm):
     flight_date = forms.DateField(
         label='Когда летим',
-        # установили валидатор минимальной даты на бэкенде
-        # внимание: даже если валидатор один, он должен передаваться в списке
-        validators=[MinValueValidator(get_min_date)],
+        validators=[MinValueValidator(get_min_date), ],
         widget=DateInput(
             attrs={
-                # переопределили атрибут `type` тега инпут, чтобы 
-                # вместо поля для ввода текста отображался календарь
                 'type': 'date',
-                # установили валидатор минимальной даты на фронтенде
-                # при каждом рендеринге страницы атрибут min тега input 
-                # будет меняться в зависимости от текущей даты
                 'min': get_min_date
             }
         )
     )
+
+    currency = forms.ChoiceField(
+        choices = (
+            ('RUB', 'Российский рубль'),
+            ('USD', 'Доллар США')
+        ),
+        label='Валюта платежа'
+    )
+
+    class Meta:
+        model = Order
+        fields = ['ship', 'flight_date', 'currency']
+        widgets = {'ship': RadioSelect}
+        labels = {'ship': 'На чем летим'}
+
+    def clean(self):
+        flight_date = self.cleaned_data['flight_date']
+        ship = self.cleaned_data['ship']
+        existing_orders = Order.objects.filter(flight_date=flight_date,
+                                               ship=ship).count()
+        if existing_orders >= ship.capacity:
+            raise forms.ValidationError(
+                'К сожалению, на эту дату мест нет. '
+                'Пожалуйста, выберите другую дату или другой корабль.'
+            )
